3cd4a9f859ffc3512098cadb4d36e206958ceb97
brightonc23 <brightonc@provo.edu>
2023-04-19 15:04:48 -0600

    Fixed sh File.

--- download.sh
+++ download.sh
@@ -1,8 +1,16 @@
-wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/config.json?token=GHSAT0AAAAAACBMJIS6DNXZY2AI65CCXIPIZCAKKEQ
-wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/door_logger.py?token=GHSAT0AAAAAACBMJIS6EL7ICQK7UA2EZ6MWZCAKOWQ
-wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/door_parser.py?token=GHSAT0AAAAAACBMJIS6FEOHWBQEZ7GCRJ6CZCAKPAA
+mkdir door_timer
+
+wget https://github.com/Provo-City-School-District/DoorTimer/raw/main/config.json
+wget https://github.com/Provo-City-School-District/DoorTimer/raw/main/door_logger.py
+wget https://github.com/Provo-City-School-District/DoorTimer/raw/main/door_parser.py
+
 mkdir lib
 cd lib
-wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/lib/date.py?token=GHSAT0AAAAAACBMJIS6TILRLRGENA6A5WI6ZCAKPTA
-wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/lib/gpio_loader.py?token=GHSAT0AAAAAACBMJIS7IMLQGJZPCOW7UGR6ZCAKP3Q
+
+wget https://github.com/Provo-City-School-District/DoorTimer/raw/main/lib/date.py
+wget https://github.com/Provo-City-School-District/DoorTimer/raw/main/lib/gpio_loader.py
+
 cd ..
+
+# Remove myself
+rm -- "$0"
