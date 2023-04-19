050029475d4e66bc0dac50b6a0881e8f6c732a0a
brightonc23 <brightonc@provo.edu>
2023-04-19 14:51:09 -0600

    Added sh file to assist in download process

---
+++ download.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/config.json?token=GHSAT0AAAAAACBMJIS6DNXZY2AI65CCXIPIZCAKKEQ
+wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/door_logger.py?token=GHSAT0AAAAAACBMJIS6EL7ICQK7UA2EZ6MWZCAKOWQ
+wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/door_parser.py?token=GHSAT0AAAAAACBMJIS6FEOHWBQEZ7GCRJ6CZCAKPAA
+mkdir lib
+cd lib
+wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/lib/date.py?token=GHSAT0AAAAAACBMJIS6TILRLRGENA6A5WI6ZCAKPTA
+wget https://raw.githubusercontent.com/Provo-City-School-District/DoorTimer/main/lib/gpio_loader.py?token=GHSAT0AAAAAACBMJIS7IMLQGJZPCOW7UGR6ZCAKP3Q
+cd ..
